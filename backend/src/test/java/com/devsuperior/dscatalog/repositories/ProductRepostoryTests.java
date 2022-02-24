04ed88e6e5a65044d3a4cd93bffd27db858f0ad8
Lucas Galvao <T0038232@vli-logistica.com.br>
2022-02-24 00:53:07 -0300

    Pattern factory for objects

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepostoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepostoryTests.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.repositories;
 
 import com.devsuperior.dscatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,16 +16,24 @@ public class ProductRepostoryTests {
     @Autowired
     private ProductRepository repository;
 
+    private long existingId;
+    private long nonExistingId;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        repository.deleteById(1L);
-        Optional<Product> result = repository.findById(1L);
+        repository.deleteById(existingId);
+        Optional<Product> result = repository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
     }
 
     @Test void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
         Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
-            long nonExistingId = 1000L;
             repository.deleteById(nonExistingId);
         });
     }
