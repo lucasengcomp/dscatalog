62194798c11d70cea51864d071ee55d094910011
Lucas Galvao <T0038232@vli-logistica.com.br>
2022-02-24 00:45:04 -0300

    Test - Should delete object when id no exists

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepostoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepostoryTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Optional;
 
@@ -19,5 +20,12 @@ public class ProductRepostoryTests {
         repository.deleteById(1L);
         Optional<Product> result = repository.findById(1L);
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+            long nonExistingId = 1000L;
+            repository.deleteById(nonExistingId);
+        });
     }
 }
