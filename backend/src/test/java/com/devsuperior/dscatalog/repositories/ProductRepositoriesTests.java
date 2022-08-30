7b7535c8848f2235656e40bef270973af717e1f6
Lucas Galvao <lucas.engcomp@outlook.com>
2022-08-30 17:29:09 -0300

    Test Delete when nonExistingId

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoriesTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoriesTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 @DataJpaTest
 public class ProductRepositoriesTests {
@@ -18,5 +19,13 @@ public class ProductRepositoriesTests {
         var result = repository.findById(existingId);
 
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+        long nonExistingId = 1000L;
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+            repository.deleteById(nonExistingId);
+        });
     }
 }
