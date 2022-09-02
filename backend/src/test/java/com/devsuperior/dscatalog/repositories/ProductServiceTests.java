c9266e6b8b669e5555cb894516fc8e168a357d60
Lucas Galvao <lucas.engcomp@outlook.com>
2022-09-02 11:11:04 -0300

    Test - should throw DatabaseException when dependent id

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductServiceTests.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.repositories;
 
 import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -17,6 +19,7 @@ public class ProductServiceTests {
 
     private Long existingId;
     private Long nonExistingId;
+    private Long dependentId;
 
     @InjectMocks
     private ProductService service;
@@ -28,9 +31,19 @@ public class ProductServiceTests {
     void setUp() throws Exception {
         existingId = 1L;
         nonExistingId = 1000L;
+        dependentId = 4L;
 
         Mockito.doNothing().when(repository).deleteById(existingId);
         Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+        Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+    }
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+        Assertions.assertThrows(DatabaseException.class, () -> {
+            service.delete(dependentId);
+        });
+        Mockito.verify(repository, Mockito.times(1)).deleteById(dependentId);
     }
 
     @Test
