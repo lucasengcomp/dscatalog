14887a4627d2b3633f5e2c502535b756d8d46ccc
Lucas Galvao <lucas.engcomp@outlook.com>
2022-11-27 13:43:51 -0300

    :sparkles: Custom constraint validator

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,46 @@
+package com.devsuperior.dscatalog.services.validation;
+
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.resources.exeptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = repository.findByEmail(dto.getEmail());
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email já existe"));
+        }
+
+            // Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
+
+        validateErrors(context, list);
+        return list.isEmpty();
+    }
+
+    private static void validateErrors(ConstraintValidatorContext context, List<FieldMessage> list) {
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+    }
+}
